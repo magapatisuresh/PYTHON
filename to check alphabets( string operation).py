9c7f1e2b5f39142de90bac58190959b1947624d4
magapatisuresh <93205460+magapatisuresh@users.noreply.github.com>
2021-10-30 05:08:30 -0700

    Add files via upload

---
+++ to check alphabets( string operation).py
@@ -0,0 +1,6 @@
+#take a alphabet from user and find out whether it is vowel or constant
+a=input("Enter a alphabet")
+if a=='a' or a == 'e' or a == 'i' or a == 'o' or a == 'u':
+    print("IT is a vowel")
+else :
+    print("It is a consonant")
