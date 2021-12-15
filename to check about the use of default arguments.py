1f8dd847fc7823ea210cd82d5f360daf3063c30f
magapatisuresh <93205460+magapatisuresh@users.noreply.github.com>
2021-12-15 17:35:46 +0530

    Add files via upload

---
+++ to check about the use of default arguments.py
@@ -0,0 +1,4 @@
+# To check about the use of default arguments
+def location(city, country="india", continent="asia"):
+    print(city, "Is located in",country,"which is in ",continent)
+location("chennai")
---
+++ to know about the doc strings function.py
@@ -0,0 +1,11 @@
+# To know about the doc strings function
+def even_odd(n):
+    '''Description on function even_odd
+parameters:n, an integer
+returns:True, if the number n is even
+        False, the number is odd'''
+
+    if n%2 == 0:
+        return True
+    else :
+        return False
