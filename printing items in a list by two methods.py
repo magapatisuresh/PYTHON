9538e06f78464d13bfef4d0995a60680b60a0ef5
magapatisuresh <93205460+magapatisuresh@users.noreply.github.com>
2021-12-17 17:45:27 +0530

    Add files via upload

---
+++ printing items in a list by two methods.py
@@ -0,0 +1,16 @@
+#To printing items of a list by two methods
+l=[10, 20, 30, 40, 50]
+
+
+#1.element based access:
+for each in l:
+    print(each)
+
+
+print()
+
+
+
+# 2. Indexed based access:
+for i in range (len(l)):
+    print(l[i])
